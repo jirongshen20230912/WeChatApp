af2b46acab63e86cf14a8e44e425d04b6414983a
shen ji rong <334269511@qq.com>
2018-05-29 22:40:36 +0800

    音乐播放

--- ReaderMovie/pages/posts/post-detail/post-detail.js
+++ ReaderMovie/pages/posts/post-detail/post-detail.js
@@ -1,12 +1,13 @@
 // pages/posts/post-detail/post-detail.js
 var postsData = require('../../../data/posts-data.js')
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    isPlayingMusic: false
   },
 
   /**
@@ -34,11 +35,65 @@ Page({
       wx.setStorageSync('posts_collected', postsCollected);
     }
 
+    if (app.globalData.g_isPlayingMusic && app.globalData.g_currentMusicPostId
+      === postId) {
+      this.setData({
+        isPlayingMusic: true
+      })
+    }
+    this.setMusicMonitor();
+
   },
+
+  setMusicMonitor: function () {
+    //点击播放图标和总控开关都会触发这个函数
+    var that = this;
+    wx.onBackgroundAudioPlay(function (event) {
+      var pages = getCurrentPages();
+      var currentPage = pages[pages.length - 1];
+      if (currentPage.data.currentPostId === that.data.currentPostId) {
+        // 打开多个post-detail页面后，每个页面不会关闭，只会隐藏。通过页面栈拿到到
+        // 当前页面的postid，只处理当前页面的音乐播放。
+        if (app.globalData.g_currentMusicPostId == that.data.currentPostId) {
+          // 播放当前页面音乐才改变图标
+          that.setData({
+            isPlayingMusic: true
+          })
+        }
+        // if(app.globalData.g_currentMusicPostId == that.data.currentPostId )
+        // app.globalData.g_currentMusicPostId = that.data.currentPostId;
+      }
+      app.globalData.g_isPlayingMusic = true;
+
+    });
+    wx.onBackgroundAudioPause(function () {
+      var pages = getCurrentPages();
+      var currentPage = pages[pages.length - 1];
+      if (currentPage.data.currentPostId === that.data.currentPostId) {
+        if (app.globalData.g_currentMusicPostId == that.data.currentPostId) {
+          that.setData({
+            isPlayingMusic: false
+          })
+        }
+      }
+      app.globalData.g_isPlayingMusic = false;
+      // app.globalData.g_currentMusicPostId = null;
+    });
+    wx.onBackgroundAudioStop(function () {
+      that.setData({
+        isPlayingMusic: false
+      })
+      app.globalData.g_isPlayingMusic = false;
+      // app.globalData.g_currentMusicPostId = null;
+    });
+  },
+
   onColletionTap: function (event) {
     // this.getPostsCollectedSyc();
     this.getPostsCollectedAsy();
   },
+
+
   getPostsCollectedAsy: function () {
     var that = this;
     wx.getStorage({
@@ -117,5 +172,30 @@ Page({
         })
       }
     })
+  },
+  onMusicTap: function (event) {
+    var currentPostId = this.data.currentPostId;
+    var postData = postsData.postList[currentPostId];
+    var isPlayingMusic = this.data.isPlayingMusic;
+    if (isPlayingMusic) {
+      wx.pauseBackgroundAudio();
+      this.setData({
+        isPlayingMusic: false
+      })
+      // app.globalData.g_currentMusicPostId = null;
+      app.globalData.g_isPlayingMusic = false;
+    }
+    else {
+      wx.playBackgroundAudio({
+        dataUrl: postData.music.url,
+        title: postData.music.title,
+        coverImgUrl: postData.music.coverImg,
+      })
+      this.setData({
+        isPlayingMusic: true
+      })
+      app.globalData.g_currentMusicPostId = this.data.currentPostId;
+      app.globalData.g_isPlayingMusic = true;
+    }
   }
 })
